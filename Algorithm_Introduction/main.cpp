8d7682da0b82a8d9ca310f37a813c33af29e1bb7
cooperwenji <378015736@qq.com>
2017-10-14 21:20:28 +0800

    avl_tree_final

---
+++ Algorithm_Introduction/main.cpp
@@ -0,0 +1,41 @@
+#include"avl_tree.h"
+
+static int arr[] = { 3, 2, 1, 4, 5, 6, 7, 16, 15, 14, 13, 12,11,10,8,9 };
+
+int main()
+{
+	int i, ilen;
+	AVLTree<int>* tree = new AVLTree<int>();
+
+	ilen = sizeof(arr) / sizeof(arr[0]);
+	std::cout << "依次添加： ";
+	for (int i = 0; i < ilen; i++)
+	{
+		std::cout << arr[i] << "  ";
+		tree->insert(arr[i]);
+	}
+
+	std::cout << "\n== 前序遍历： ";
+	tree->preOrder();
+
+	std::cout << "\n== 中序遍历： ";
+	tree->inOrder();
+
+	std::cout << "\n== 后序遍历： ";
+	tree->postOrder();
+	std::cout << std::endl;
+    
+	std::cout << "==高度: " << tree->height() << std::endl;
+	std::cout << "==最小值" << tree->minimum() << std::endl;
+	std::cout << "==最大值" << tree->maximum() << std::endl;
+
+	tree->remove(8);
+	std::cout << "\n== 删除根节点：" << "8";	
+	std::cout << "\n==高度： " << tree->height();
+	std::cout << "\n==中序遍历： ";
+	tree->inOrder();
+
+	tree->destroy();
+
+	system("pause");
+}
